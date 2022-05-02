3f751c05bb0d5cf28c4a02f08e51f4581b624bc9
Марина Бобрецова <marinabobrecova@MacBook-Air-Marina.local>
2022-05-02 23:17:17 +0300

    Add new file

---
+++ testcat.py
@@ -0,0 +1,8 @@
+from cat import Cat
+
+cat = Cat('Барон', 'мальчик', 2)
+cat_2 = Cat('Cэм', 'мальчик', 2)
+
+print(f'Имя: {cat.name}\nВозраст: {cat.gender}\nВозраст: {cat.age} года\n')
+
+print(f'Имя: {cat_2.name}\nПол: {cat_2.gender}\nВозраст: {cat_2.age}')
